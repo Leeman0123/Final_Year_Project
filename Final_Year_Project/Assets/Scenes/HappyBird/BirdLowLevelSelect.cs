4f3ab8c8880842aafcbbb6cd9e847be4ec079d7f
kelvin <75073141+Kelvin-123@users.noreply.github.com>
2021-04-11 04:07:03 +0800

    Test

---
+++ Final_Year_Project/Assets/Scenes/HappyBird/BirdLowLevelSelect.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Firebase.Database;
+using UnityEngine.UI;
+using System.IO;
+
+public class BirdLowLevelSelect : MonoBehaviour {
+
+    public Text CoinText;
+    public Button[] levelbuttons;
+    public GameObject NotEnoughCoinPanel;
+    int Coins;
+    int levelReached;
+    [SerializeField] GameObject CheckAuth;
+
+    private string userID;
+    private CheckAuthentication script;
+    private DatabaseReference reference;
+
+   
+    private void Start() {
+        script = CheckAuth.GetComponent<CheckAuthentication>();
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        userID = script.GetUserId();
+
+
+        StartCoroutine(GetPlayerCoins());
+        StartCoroutine(GetBirdLevel());
+        
+        //reference.Child("students").Child(userID).Child("BirdLevel").SetValueAsync(1);
+
+        //levelReached = PlayerPrefs.GetInt("BirdLowLevel", 1);
+        //Coins = PlayerPrefs.GetInt("BirdCoin", 15);
+
+        /*for (int i = 0; i < levelbuttons.Length; i++) {
+            if (i + 1 > levelReached)
+                levelbuttons[i].interactable = false;
+        }*/
+    }
+    private void Update()
+    {
+        CoinText.text = "Coin : " + Coins;
+    }
+
+    private IEnumerator GetPlayerCoins()
+    {
+        var getTask = FirebaseDatabase.DefaultInstance
+        .GetReference("students")
+        .Child(script.GetUserId())
+        .GetValueAsync();
+        yield return new WaitUntil(() => getTask.IsCompleted || getTask.IsFaulted);
+        if (getTask.IsCompleted)
+        {
+            Dictionary<string, object> results = (Dictionary<string, object>)getTask.Result.Value;
+            Coins = int.Parse(results["coins"].ToString());
+            Debug.Log("Coins = " + Coins);
+            //levelReached = int.Parse(results["BirdLevel"].ToString());
+            //string coins = results["coins"].ToString();
+            //Debug.Log(coins);
+            //SetPlayerCoins(Coins, (int)(Score * 0.05));
+        }
+    }
+
+    private IEnumerator GetBirdLevel()
+    {
+        var getTask = FirebaseDatabase.DefaultInstance
+        .GetReference("students")
+        .Child(script.GetUserId())
+        .GetValueAsync();
+        yield return new WaitUntil(() => getTask.IsCompleted || getTask.IsFaulted);
+        if (getTask.IsCompleted)
+        {
+            Dictionary<string, object> results = (Dictionary<string, object>)getTask.Result.Value;
+            levelReached = int.Parse(results["BirdLevel"].ToString());
+            Debug.Log("LevelReached = " + levelReached);
+            for (int i = 0; i < levelbuttons.Length; i++)
+            {
+                if (i + 1 > levelReached)
+                    levelbuttons[i].interactable = false;
+            }
+
+            //string coins = results["coins"].ToString();
+            //Debug.Log(coins);
+            //SetPlayerCoins(Coins, (int)(Score * 0.05));
+        }
+    }
+
+    public void Select(string levelName) {
+        if(Coins >= 10)
+        {
+            Coins -= 10;
+
+            reference.Child("students").Child(userID).Child("coins").SetValueAsync(Coins);
+
+            //PlayerPrefs.SetInt("BirdCoin", NewCoin);
+            SceneManager.LoadScene(levelName);
+        }
+        else
+        {
+            NotEnoughCoinPanel.SetActive(true);
+        }
+    }
+    public void GoQuiz()
+    {
+        SceneManager.LoadScene("MathQuiz");
+    }
+    public void BackMenu()
+
+    {
+        NotEnoughCoinPanel.SetActive(false);
+    }
+    public void BackHome()
+
+    {
+        SceneManager.LoadScene("Bird_HomePage");
+    }
+
+}
---
+++ Final_Year_Project/Assets/Scenes/HappyBird/Quiz2/MathMessagePanelControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MathMessagePanelControl : MonoBehaviour
+{
+    [SerializeField] int questionsCount;
+    [SerializeField] int questionsSeconds;
+    [SerializeField] Text message;
+    [SerializeField] Button startBtn;
+    [SerializeField] Button closeBtn;
+    [SerializeField] GameObject loadingPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message.text = string.Format("This quiz has total {0} questions.\n\n" +
+            "Each question has {1} seconds to answer. \n\n" +
+            "Get a coin for every correct answer", questionsCount, questionsSeconds);
+        startBtn.onClick.AddListener(() =>
+        {
+            GameObject.Find("BarBackground").GetComponentInChildren<MathBarScript>().StartTimer();
+            gameObject.SetActive(false);
+        });
+        closeBtn.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+            //loadingPanel.SetActive(true);
+            SceneManager.LoadScene("Bird_Low_LevelSelect");
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
