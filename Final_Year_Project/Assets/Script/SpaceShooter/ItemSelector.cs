f2cbea6fadcdb4cec8abc4c21c4fbe8535e49430
p5p53c <p5p53c@gmail.com>
2021-04-12 20:25:00 +0800

    Update itemAmount Firebase path
    change itemAmount Firebase path from spaceshooter/itemAmount/$userID to students/$userID/itemAmount

--- Final_Year_Project/Assets/Script/SpaceShooter/ItemSelector.cs
+++ Final_Year_Project/Assets/Script/SpaceShooter/ItemSelector.cs
@@ -1,4 +1,5 @@
 ﻿using Firebase.Database;
+using Firebase.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,7 @@ public class ItemSelector : MonoBehaviour
     public Sprite enableImage, disableImage, selectedImage;
 
     private CheckAuthentication script;
+    private DatabaseReference reference;
     private string userID;
 
     // Start is called before the first frame update
@@ -26,9 +28,10 @@ public class ItemSelector : MonoBehaviour
     {
         itemInstance = this;
         script = GameObject.Find("CheckAuth").GetComponent<CheckAuthentication>();
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
         userID = script.GetUserId();
         Debug.Log(userID);
-        StartCoroutine(GetItemAmount());
+        StartCoroutine(ShowItemAmount());
     }
 
     public void ChangeColor(int itemselected) {
@@ -43,7 +46,8 @@ public class ItemSelector : MonoBehaviour
         }
     }
 
-    private void ShowItemAmount() {
+    private IEnumerator ShowItemAmount() {
+        yield return StartCoroutine(GetItemAmount());
         for (int i = 0; i <= 2; i++) {
             itemText[i].text = "Owned : " + itemOwned[i];
             itemEnable[i] = false;
@@ -56,26 +60,26 @@ public class ItemSelector : MonoBehaviour
 
     IEnumerator GetItemAmount() {
         var getTask = FirebaseDatabase.DefaultInstance
-        .GetReference("SpaceShooter")
-        .Child("ItemAmount")
+        .GetReference("students")
         .Child(userID)
+        .Child("ItemAmount")
         .GetValueAsync();
         yield return new WaitUntil(() => getTask.IsCompleted || getTask.IsFaulted);
         if (getTask.IsCompleted) {
             Dictionary<string, object> results = (Dictionary<string, object>)getTask.Result.Value;
+            Debug.Log(results);
             itemOwned[0] = int.Parse(results["Invincible"].ToString());
             itemOwned[1] = int.Parse(results["SpecialMode"].ToString());
             itemOwned[2] = int.Parse(results["DoubleCoin"].ToString());
         }
-        ShowItemAmount();
     }
 
     public IEnumerator SetInvincibleAmount(int change) {
         itemOwned[0] = itemOwned[0] + change;
         var DBTask = script.DBreference
-            .Child("SpaceShooter")
-            .Child("ItemAmount")
+            .Child("students")
             .Child(userID)
+            .Child("ItemAmount")
             .Child("Invincible")
             .SetValueAsync(itemOwned[0]);
         yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
@@ -89,9 +93,9 @@ public class ItemSelector : MonoBehaviour
     public IEnumerator SetSpecialModeAmount(int change) {
         itemOwned[1] = itemOwned[1] + change;
         var DBTask = script.DBreference
-            .Child("SpaceShooter")
-            .Child("ItemAmount")
+            .Child("students")
             .Child(userID)
+            .Child("ItemAmount")
             .Child("SpecialMode")
             .SetValueAsync(itemOwned[1]);
         yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
@@ -105,9 +109,9 @@ public class ItemSelector : MonoBehaviour
     public IEnumerator SetDoubleCoinAmount(int change) {
         itemOwned[2] = itemOwned[2] + change;
         var DBTask = script.DBreference
-            .Child("SpaceShooter")
-            .Child("ItemAmount")
+            .Child("students")
             .Child(userID)
+            .Child("ItemAmount")
             .Child("DoubleCoin")
             .SetValueAsync(itemOwned[2]);
         yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
@@ -116,5 +120,17 @@ public class ItemSelector : MonoBehaviour
         } else {
             Debug.Log("Double Coin Amount Updated. New Amount : " + itemOwned[2]);
         }
+    }
+
+    public void SetItemAmount(string itemName) {
+        FirebaseDatabase dbInstance = FirebaseDatabase.DefaultInstance;
+        dbInstance.GetReference("students").Child(userID).Child("ItemAmount").GetValueAsync().ContinueWithOnMainThread(task => {
+            if (task.IsCompleted) {
+                DataSnapshot snapshot = task.Result;
+                int temp = int.Parse(snapshot.Child(itemName).Value.ToString());
+                temp--;
+                reference.Child("students").Child(userID).Child("ItemAmount").Child(itemName).SetValueAsync(temp);
+            }
+        });
     }
 }
